8a3d8811c3ab904063710f7ef0fa175dfed7358d
Wagner <wagnersr92@gmail.com>
2022-12-02 21:07:40 -0300

    Initial Commit

---
+++ ArquiteturaSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArquiteturaSwift
+//
+//  Created by Wagner Rodrigues on 02/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
